51f26cebff61296178304636ae527c5e79df3aaf
Joosee003 <josettesonia@gmail.com>
2026-09-15 02:24:52 +0200

    Build agency control center and reliable restaurant onboarding

---
+++ app/admin/restaurantes/[id]/page.tsx
@@ -0,0 +1,5 @@
+import { RestaurantWorkspace } from "../../components/AgencyViews";
+export default async function RestaurantPage({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+  return <RestaurantWorkspace restaurantId={id}/>;
+}
